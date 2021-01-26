91c7e90fcddd8cd0fda576e3b907514c2ff18de2
salahoddin <salahoddinsambutuan@gmail.com>
2021-01-26 17:53:26 +0800

    connecting to database, modeling data, fetch products from database, custom error handling

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs'
+const users = [
+	{
+		name: 'Admin User',
+		email: 'admin@example.com',
+		password: bcrypt.hashSync('123456', 10),
+		isAdmin: true,
+	},
+	{
+		name: 'John Doe',
+		email: 'johndoe@example.com',
+		password: bcrypt.hashSync('123456', 10),
+		isAdmin: false,
+	},
+	{
+		name: 'Jane Doe',
+		email: 'janedoe@example.com',
+		password: bcrypt.hashSync('123456', 10),
+		isAdmin: false,
+	},
+]
+
+export default users
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,30 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import products from './data/products.js'
+import color from 'colors'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
 dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
 	res.send('API is Running')
 })
 
-app.get('/api/products', (req, res) => {
-	res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((p) => p._id === req.params.id)
-	res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
 	PORT,
-	console.log(`SERVER RUNNING IN ${process.env.NODE_ENV} MODE ON PORT ${PORT}`)
+	console.log(
+		`SERVER RUNNING IN ${process.env.NODE_ENV} MODE ON PORT ${PORT}`.yellow.bold
+	)
 )
