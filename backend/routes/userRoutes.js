4f6c10fb3c3d203789775ed014af9ae28fe5cf88
salahoddin <salahoddinsambutuan@gmail.com>
2021-01-30 10:52:37 +0800

    user login action and reducers, login screen, username in navbar, logout funtionality, register action and reducer, profile update backend, profile screen (user details), update user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
 	authUser,
 	getUserProfile,
 	registerUser,
+	updtateUserProfile,
 } from '../controllers/userController.js'
 import { protect } from '../middlewares/authMiddleware.js'
 
@@ -13,6 +14,11 @@ router.route('/').post(registerUser)
 router.post('/login', authUser)
 
 // protect middleware will run when it hit this route
-router.route('/profile').get(protect, getUserProfile)
+
+// get(.get) user profile and/or update user profile(.put)
+router
+	.route('/profile')
+	.get(protect, getUserProfile)
+	.put(protect, updtateUserProfile)
 
 export default router
