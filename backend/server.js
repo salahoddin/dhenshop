19d10b783096c2b657f93fd7d6b852c234e8c454
salahoddin <salahoddinsambutuan@gmail.com>
2021-02-08 02:24:11 +0800

    ignore node modules

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import path from 'path'
-import dotenv from 'dotenv'
 import express from 'express'
-import color from 'colors'
+import dotenv from 'dotenv'
+import colors from 'colors'
 import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
 dotenv.config()
 
@@ -20,7 +21,6 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 
-// this will allow us accept json from request body
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
@@ -32,7 +32,6 @@ app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-// to make uploads folder accessible
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
@@ -49,13 +48,13 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
+
 app.listen(
 	PORT,
 	console.log(
-		`SERVER RUNNING IN ${process.env.NODE_ENV} MODE ON PORT ${PORT}`.yellow.bold
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
 	)
 )
